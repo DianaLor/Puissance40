784a3e914911ab9e6a4f778ef6eb3a98b98637b1
FifaIiana <fifaliana.randriamitandrina@eleve.egd.mg>
2020-02-21 12:29:21 +0300

    Modification

---
+++ horiz_.py
@@ -0,0 +1,56 @@
+  
+def horiz(gril,lig,col,j):
+    m03=[]
+    m25=[]
+    m14=[]
+    u=2
+    drapeau=False
+    if j==1:
+        u=2
+    else:
+        u=1
+    if gril[lig][col]==j:
+            for r in range(4):
+                m03.append(gril[lig][r])
+            for k in range(1,5):
+                m14.append(gril[lig][k])
+            for p in range(2,6):
+                m25.append(gril[lig][p])
+                
+            if u not in m03 and 0 not in m03 and drapeau==False:
+                drapeau=True
+                
+            if u not in m14 and 0 not in m14 and drapeau==False:
+                drapeau=True
+                
+            if u not in m25 and 0 not in m25 and drapeau==False:
+                drapeau=True
+    return drapeau
+
+assert horiz([[0, 1, 2, 2, 1, 0],
+             [0, 0, 0, 0, 0, 0],
+             [0, 1, 1, 1, 1, 1],
+             [0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0]],2,2,1)==True
+
+assert horiz([[0, 1, 2, 2, 1, 0],
+             [0, 0, 0, 0, 0, 0],
+             [0, 1, 1, 0, 1, 1],
+             [0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0]],2,2,1)==False
+
+assert horiz([[0, 1, 2, 2, 1, 0],
+             [0, 0, 0, 0, 0, 0],
+             [0, 1, 1, 1, 1, 1],
+             [0, 2, 2, 2, 2, 0],
+             [0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0]],3,3,2)==True
+
+assert horiz([[0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0]],1,4,1)==False 
