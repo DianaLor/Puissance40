f9b8e96a41e45668aca2f94c22e3063056eaa2ff
FifaIiana <60935184+FifaIiana@users.noreply.github.com>
2020-03-02 06:14:45 +0100

    Update victoire.py

--- victoire.py
+++ victoire.py
@@ -1,8 +1,15 @@
-def victoire(gril,j,lig,col):
-    if horiz(gril, j, lig, col) == True or vert(gril, j, lig, col) == True or diag_haut(gril, j, lig, col) ==True or diag_bas(gril, j, lig, col)==True:
+def victoire(gril,j):
+    for lig in range(6):
+        for col in range(7):
+            if horiz(gril, j, lig, col) == True or vert(gril, j, lig, col) == True or diag_haut(gril, j, lig, col) ==True or diag_bas(gril, j, lig, col)==True:
+                n+=1
+
+    if n!=0:
         return True
     else:
         return False
+
+
     
 assert victoire([[0,0,1,0,0,0,0],
       [0,0,1,0,0,1,0],
