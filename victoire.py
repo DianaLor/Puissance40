7651a0ca242c49f74044821eb67d3e6e0bfaffc3
WjLliS <60876619+WjLliS@users.noreply.github.com>
2020-03-03 19:44:19 +0300

    Update victoire.py

--- victoire.py
+++ victoire.py
@@ -1,15 +1,15 @@
 def victoire(gril,j):
-    n=0
-    for lig in range(6):
-        for col in range(7):
-            if horiz(gril, j, lig, col) == True or vert(gril, j, lig, col) == True or diag_haut(gril, j, lig, col) ==True or diag_bas(gril, j, lig, col)==True:
-                n+=1
-
-    if n!=0:
-        return True
-    else:
-        return False
-
+    for y in range(0,6):
+        for x in range (0,7):
+            if horiz(gril,j,y,x)==True:
+                return True
+            if vert(gril,j,y,x)==True:
+                return True
+            if diag_haut(gril,j,x,y)==True:
+                return True
+            if diag_bas(gril,j,x,y)==True:
+                return True
+    return False
 
     
 assert victoire([[0,0,1,0,0,0,0],
@@ -17,16 +17,16 @@ assert victoire([[0,0,1,0,0,0,0],
       [1,0,1,2,1,2,0],
       [1,1,1,1,2,2,0],
       [2,2,1,2,1,2,0],
-      [2,1,1,1,1,2,2]],2,2,3)== True
+      [2,1,1,1,1,2,2]],2)== True
 assert victoire([[0,0,0,0,0,0,0],
       [0,0,0,0,0,0,0],
       [0,0,0,0,0,0,0],
       [0,0,0,0,0,0,0],
       [0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0]],1,1,5)== False
+      [0,0,0,0,0,0,0]],1)== False
 assert victoire([[0,0,0,0,0,0,0],
       [0,0,0,0,0,0,0],
       [0,0,0,1,0,0,0],
       [0,0,0,1,0,0,0],
       [0,0,0,1,0,0,0],
-      [0,0,0,1,0,0,0]],1,5,3)== True
+      [0,0,0,1,0,0,0]],1)== True
