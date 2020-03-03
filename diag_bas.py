40162efe58075fcb86d9b85e69381f626df17d95
WjLliS <60876619+WjLliS@users.noreply.github.com>
2020-03-03 22:26:09 +0300

    Update diag_bas.py

--- diag_bas.py
+++ diag_bas.py
@@ -1,50 +1,43 @@
-def diag_haut(gril,col,lig,j):  
-    x1=lig
-    y1=col
-    x2=lig
-    y2=col
-    fin=[]
-    fin2=[]
-    for i in range (6):
-        if x1<=5 and y1<=6 and gril[x1][y1]==j: 
-                fin.append(gril[x1][y1])
-                x1 -= 1
-                y1 += 1
-    for i in range (6):
-        if x2<=5 and y2<=6:
-            if gril[x2][y2]==j:
-                fin2.append(gril[x2][y2])
-                x2 -= 1
-                y2 -= 1
-    if len(fin) >= 4 or len(fin2)>= 4 :
-        return True
-    else :
+def diag_bas(gril,j,lig,col):
+    x1 = lig
+    y1 = col
+    x2 = lig
+    y2 = col
+    fin = []
+    fin2 = []
+    if lig > 2 :
         return False
+    else:
+        for i in range (6) :
+            if x1<=5 and y1<=6 and gril[x1][y1] == j :
+                fin.append(gril[x1][y1])
+                x1 += 1
+                y1 += 1
+        for i in range (6)  :
+            if x2<=5 and y2<=6 and gril[x2][y2] == j:
+                fin2.append(gril[x2][y2])
+                x2 += 1
+                y2 -= 1
+        if len(fin) >= 4 or len(fin2) >= 4 :
+            return True 
+        else :
+            return False
         
-assert diag_haut([[0,2,1,0,1,0,0],
-                 [0,0,0,1,0,0,0],
-                 [0,0,1,2,1,0,2],
-                 [0,1,2,1,2,2,0],
-                 [0,2,1,0,0,1,0],
-                 [2,1,1,2,1,1,0]],0,5,2)==True
-
-assert diag_haut([[0,2,1,1,1,0,0],
-                 [0,0,0,1,1,0,0],
-                 [0,0,1,2,1,1,2],
-                 [0,1,0,1,2,2,1],
-                 [0,2,1,0,0,1,0],
-                 [2,1,1,2,1,1,0]],6,3,1)==True
-
-assert diag_haut([[0,2,1,0,1,0,0],
-                 [0,0,0,1,0,1,0],
-                 [0,0,1,2,1,0,2],
-                 [0,1,0,1,2,2,0],
-                 [0,2,1,0,0,1,0],
-                 [2,1,1,2,1,1,0]],2,4,1)==True
-
-assert diag_haut([[0,2,1,0,1,0,0],
-                 [0,0,0,1,0,0,0],
-                 [0,0,1,2,1,0,2],
-                 [0,1,0,1,2,2,0],
-                 [0,2,1,0,0,1,0],
-                 [2,1,1,2,1,1,0]],2,4,1)==False
+assert diag_bas([[0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0]],1,6,6)== False
+assert diag_bas([[0,0,1,0,0,0,0],
+      [0,0,1,0,0,1,0],
+      [1,0,1,2,1,2,0],
+      [1,1,1,1,2,2,0],
+      [2,2,1,2,1,2,0],
+      [2,1,1,1,1,2,2]],2,2,3)== True
+assert diag_bas([[0,0,1,0,0,0,0],
+      [0,0,1,0,0,1,0],
+      [1,0,1,2,1,2,0],
+      [1,1,1,1,2,2,0],
+      [2,2,1,2,1,2,0],
+      [2,1,1,1,1,2,2]],2,4,3)== False
